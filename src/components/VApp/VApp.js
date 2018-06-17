d6e995243c1f4f9f3c6b1f1705c3264ef6f768d1
Alireza Asgharizadeh <alireza4050@gmail.com>
2018-06-18 00:24:02 +0430

    For now et dir="rtl" on html element instead

--- src/components/VApp/VApp.js
+++ src/components/VApp/VApp.js
@@ -33,9 +33,6 @@ export default {
       return {
         [`theme--${this.dark ? 'dark' : 'light'}`]: true
       }
-    },
-    dir () {
-      return this.$vuetify.rtl ? 'rtl' : 'ltr'
     }
   },
 
@@ -53,7 +50,7 @@ export default {
     const data = {
       staticClass: 'application',
       'class': this.classes,
-      attrs: { 'data-app': true, dir: this.dir },
+      attrs: { 'data-app': true },
       domProps: { id: this.id },
       directives: [{
         name: 'resize',
