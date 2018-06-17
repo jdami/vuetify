dcfd3a9b1bbe54ba0b1a03f32140e9fb1718f1f8
Alireza Asgharizadeh <alireza4050@gmail.com>
2018-06-18 00:39:03 +0430

    Moved syncPageDirection function to util/helpers

--- src/components/VApp/VApp.js
+++ src/components/VApp/VApp.js
@@ -7,8 +7,6 @@ import AppBreakpoint from './mixins/app-breakpoint'
 // Directives
 import Resize from '../../directives/resize'
 
-import { syncPageDirection } from '../../util/helpers'
-
 /* @vue/component */
 export default {
   name: 'v-app',
@@ -41,12 +39,10 @@ export default {
   watch: {
     dark () {
       this.$vuetify.dark = this.dark
-    },
-    '$vuetify.rtl': setPageDirection
+    }
   },
 
   mounted () {
-    setPageDirection()
     this.$vuetify.dark = this.dark
   },
 
