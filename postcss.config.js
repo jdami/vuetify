9ee25408d35ef0f91005f5b8bc6c59910e26c5ed
Alireza Asgharizadeh <alireza4050@gmail.com>
2018-06-18 07:55:41 +0430

    Added postcss-rtl

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,13 @@
 const autoprefixer = require('autoprefixer')
 const mqpacker = require('css-mqpacker')
+const rtl = require('postcss-rtl')
 
-module.exports = (ctx) => ({
+module.exports = ctx => ({
   plugins: [
     autoprefixer({
       browsers: ['>0.5%', 'last 2 versions', 'not dead', 'not op_mini all']
     }),
+    rtl(),
     mqpacker()
   ]
 })
